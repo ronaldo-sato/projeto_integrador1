62f587096cee4639f0d9ffd4315236fd7e297451
Ronaldo Mitsuo Sato <ronaldo.sato@gmail.com>
2026-04-23 12:49:32 -0300

    Primeira implementação app farmacia

--- farmacia/models.py
+++ farmacia/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# O nome das tabelas no Django seguem o padrão app_model
+
+
+class Farmacia(models.Model):
+
+    nome = models.CharField('Nome', max_length=100, unique=True)
+    endereco = models.CharField('Endereço', max_length=255)
+
+    def __str__(self):
+        return f'{self.nome} {self.endereco}'
