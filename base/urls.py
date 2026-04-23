1d80f11de41b370c0e3a6b60a32a81fb682565cd
Ronaldo Mitsuo Sato <ronaldo.sato@gmail.com>
2026-04-23 12:52:16 -0300

    Dividindo .css em arquivos diferentes conforme as telas, funcionamento da aplicação

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+# aplicando namespacing: todas as urls pertencem a base/
+app_name = 'base'
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
+from farmacia.models import Farmacia
+from medicamento.models import Medicamento
+
 # Create your views here.
 
 
+# def index(request):
+#     return render(
+#         request,
+#         'base/index.html')
+
+# Ter as farmácias e medicamentos cadastrados para usá-los nos selects
+# do forms de cadastro de preço
 def index(request):
-    return render(
-        request,
-        'base/index.html')
+    context = {
+        'farmacias': Farmacia.objects.all(),
+        'medicamentos': Medicamento.objects.all(),
+    }
+    return render(request, 'base/index.html', context)
