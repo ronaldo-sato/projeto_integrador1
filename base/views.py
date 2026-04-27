9898f1df37a334efb09d57eaa93f89817e816dc0
Ronaldo Mitsuo Sato <ronaldo.sato@gmail.com>
2026-04-27 18:27:19 -0300

    Alteração no contêiner de mensagens; Inclusão de preços únicos

--- base/views.py
+++ base/views.py
@@ -14,8 +14,12 @@ from medicamento.models import Medicamento
 # Ter as farmácias e medicamentos cadastrados para usá-los nos selects
 # do forms de cadastro de preço
 def index(request):
+
+    # Incluindo medicamentos_unicos: nome de medicamentos sem repetição
     context = {
         'farmacias': Farmacia.objects.all(),
         'medicamentos': Medicamento.objects.all(),
+        'medicamentos_unicos': Medicamento.objects.values('nome')
+        .distinct().order_by('nome'),
     }
     return render(request, 'base/index.html', context)
