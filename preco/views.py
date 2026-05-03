c2d0405dabaca3fa1815b35ba0ec3ae3b364d52b
Ronaldo Mitsuo Sato <ronaldo.sato@gmail.com>
2026-05-03 12:51:03 -0300

    Correções; Alterações para manutenção de estado na interação do usuário com formulário

--- preco/views.py
+++ preco/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
-
 from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse
 
 from .models import Preco, Farmacia, Medicamento
 
@@ -15,65 +15,73 @@ def cadastrar(request):
     medicamentos_unicos = Medicamento.objects.values('nome') \
         .distinct().order_by('nome')
 
-    # Para selecionar fabricantes de determinado medicamento, é preciso
-    # manter o estado das interações com o formulário
-    # Primeira interação: selecionar medicamento
-    # Segunda interação: selecionar fabricante (a partird de filtrados)
-    fabricantes_filtrados = None
-    medicamento_selecionado = None
-
-    filtrar = False
-
     if request.method == 'POST':
+
+        # Para selecionar fabricantes de determinado medicamento, é
+        # preciso manter o estado das interações com o formulário (acao)
+        # 1a interação: selecionar medicamento
+        # 2a interação: selecionar fabricante (a partir de filtrados)
+        acao = request.POST.get('acao_botao')
+        # acao = request.POST.get('acao_botao') == 'filtrar'
+
+        medicamento_selecionado = request.POST.get('medicamento_nome')
+
+        # Se a ação for filtrar, devolve os fabricantes filtrados
+        if acao == 'filtrar':  # and medicamento_selecionado:
+
+            # fabricantes_filtrados = Medicamento.objects.filter(
+            #     nome=medicamento_selecionado)
+
+            # return render(request, 'base/index.html', {
+            #     'farmacias': farmacias,
+            #     'medicamentos_unicos': medicamentos_unicos,
+            #     'fabricantes_filtrados': fabricantes_filtrados,
+            #     'medicamento_selecionado': medicamento_selecionado,
+            #     'farmacia_selecionada_id': farmacia_id,
+            #     'preco_digitado': preco_digitado,
+            # })
+
+            # Fazer com que o filtrar não vá para o topo da página,
+            # incluindo a âncora e construindo a url para o formulário
+            url = reverse('base:index') + \
+                f'?medicamento_filtro={medicamento_selecionado}#preco'
+
+            return redirect(url)
+
         # Do formulário vem os ids de farmacia e medicamento (são
         # selecionados a partir de listagem)
         farmacia_id = request.POST.get('farmacia_id')
         medicamento_id = request.POST.get('medicamento_id')
-        preco_entrada = request.POST.get('valor_preco')
+        preco_digitado = request.POST.get('valor_preco')
         # data_hora = request.POST.get('data_hora')
 
-        medicamento_selecionado = request.POST.get('medicamento_nome')
-        filtrar = request.POST.get('acao_botao') == 'filtrar'
-
-        # Quando o botão de filtrar é acionado, os fabricantes de
-        # determinado medicamento serão passados
-        if filtrar and medicamento_selecionado:
-
-            fabricantes_filtrados = Medicamento.objects.filter(
-                nome=medicamento_selecionado)
-
-            return render(request, 'base/index.html', {
-                'farmacias': farmacias,
-                'medicamentos_unicos': medicamentos_unicos,
-                'fabricantes_filtrados': fabricantes_filtrados,
-                'medicamento_selecionado': medicamento_selecionado,
-                'farmacia_selecionada_id': farmacia_id,
-                'preco_digitado': preco_entrada,
-            })
-
-    if not filtrar:
-
-        if not farmacia_id or not medicamento_id or not preco_entrada:
+        # Todos os campos são obrigatórios
+        if not farmacia_id or not medicamento_id or not preco_digitado:
 
             messages.error(
                 request,
                 'Por favor, selecione a farmácia, o medicamento e' +
                 ' informe o valor (todos os campos são obrigatórios).')
 
-            # retornando com valores selecionados
+            # Para manter o estado dos fabricantes filtrados em caso
+            # de erro
+            fabricantes = Medicamento.objects.filter(
+                nome=medicamento_selecionado) \
+                if medicamento_selecionado else None
+
             return render(request, 'base/index.html', {
                 'farmacias': farmacias,
                 'medicamentos_unicos': medicamentos_unicos,
-                'fabricantes_filtrados': fabricantes_filtrados,
+                'fabricantes_filtrados': fabricantes,
                 'medicamento_selecionado': medicamento_selecionado,
                 'farmacia_selecionada_id': farmacia_id,
-                'preco_digitado': preco_entrada,
+                'preco_digitado': preco_digitado,
             })
 
         try:
             # Converte valor para float/decimal (tratando vírgula
             # se necessário)
-            preco = float(preco_entrada.replace(',', '.'))
+            preco = float(preco_digitado.replace(',', '.'))
 
             # Cria o registro na tabela preco
             Preco.objects.create(
@@ -84,10 +92,16 @@ def cadastrar(request):
 
             messages.success(request, 'Preço cadastrado com sucesso!')
 
+            # Redirecionamento para a âncora
+            # return redirect('/#preco')
+            # Havendo cadastro melhor redirecionar para o home, para que
+            # mensagem de sucesso seja visualizada
+            return redirect('base:index')
+
         except Exception:
 
             messages.error(
-                request, 'Erro ao cadastrar preço. Verifique os dados.')
+                request, 'Erro ao cadastrar preço. Verifique o valor.')
 
     return redirect('base:index')
 
